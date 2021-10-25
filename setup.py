7882a3bf4fb720d338096897ff74615d1bedd6b3
Ken McMillan <kenmcmil@example.com>
2021-10-25 10:50:56 -0500

    bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except:
   long_description = None
 
 setup(name='ms_ivy',
-      version='1.8.9',
+      version='1.8.10',
       description='IVy verification tool',
       long_description=long_description,
       url='https://github.com/microsoft/ivy',
